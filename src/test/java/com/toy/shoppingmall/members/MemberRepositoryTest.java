993b8deeec92040ad86dd03195b6d18dc2d41bd7
Juyoung <juy5790@outlook.com>
2019-10-07 11:10:58 +0900

    feat #setting JPA 설정 - 쿼리 파라미터 로그 library 추가

---
+++ src/test/java/com/toy/shoppingmall/members/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.toy.shoppingmall.members;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    void save() {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        // then
+        assertThat(findMember).isEqualTo(member);
+    }
+}
