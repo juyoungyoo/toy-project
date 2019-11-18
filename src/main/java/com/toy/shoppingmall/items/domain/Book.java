2ce94ff6a0e409680f89770afef02f17a353cdb6
Juyoung <juy5790@outlook.com>
2019-11-18 23:22:10 +0900

    feat (item) 상품 등록

--- src/main/java/com/toy/shoppingmall/items/domain/Book.java
+++ src/main/java/com/toy/shoppingmall/items/domain/Book.java
@@ -1,17 +1,26 @@
 package com.toy.shoppingmall.items.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.toy.shoppingmall.domain.Category;
+import lombok.*;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @DiscriminatorValue("B")
 public class Book extends Item {
 
     private String author;
     private String isbn;
+
+    @Builder
+    public Book(Long id, String name, int price, int stockQuantity, List<Category> categories, String author, String isbn) {
+        super(id, name, price, stockQuantity, categories);
+        this.author = author;
+        this.isbn = isbn;
+    }
 }
